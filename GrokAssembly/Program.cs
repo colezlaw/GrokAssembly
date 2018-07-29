6117ea3ee168b7f50de310ca1b5b3df385871163
Jeremy Long <jeremy.long@gmail.com>
2018-07-28 21:29:29 -0400

    updated

--- GrokAssembly/Program.cs
+++ GrokAssembly/Program.cs
@@ -8,105 +8,99 @@ using System.Reflection;
 
 namespace GrokAssembly
 {
-	static class MainClass
-	{
-		private static string xmlSanitize(string input)
-		{
-			StringBuilder result = new StringBuilder();
-			foreach (char c in input)
-			{
-				if (XmlConvert.IsXmlChar(c))
-				{
-					result.Append(c);
-				}
-				else {
-					result.Append('?');
-				}
-			}
+    static class MainClass
+    {
+        public static int Main(string[] args)
+        {
+            int retval = 0;
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = new System.Text.UTF8Encoding(false);
+            XmlWriter writer = XmlWriter.Create(Console.OpenStandardOutput(), settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("assembly");
 
-			return result.ToString();
-		}
+            if (args.Length != 1)
+            {
+                writer.WriteStartElement("error");
+                writer.WriteString("Usage: GrokAssembly.exe <filename>");
+                writer.WriteEndElement();
+                retval = 1;
+            }
+            else if (!File.Exists(args[0]))
+            {
+                writer.WriteStartElement("error");
+                writer.WriteString("File does not exist");
+                writer.WriteEndElement();
+                retval = 2;
+            }
+            else
+            {
+                try
+                {
+                    FileVersionInfo fileInfo = FileVersionInfo.GetVersionInfo(Path.GetFullPath(args[0]));
 
-		public static int Main (string[] args)
-		{
-			int retval = 0;
-			XmlWriterSettings settings = new XmlWriterSettings ();
-			settings.Encoding = new System.Text.UTF8Encoding (false);
-			XmlWriter writer = XmlWriter.Create(Console.OpenStandardOutput(), settings);
-			writer.WriteStartDocument ();
-			writer.WriteStartElement ("assembly");
-
-			if (args.Length != 1) {
-				writer.WriteStartElement ("error");
-				writer.WriteString ("Usage: GrokAssembly.exe <filename>");
-				writer.WriteEndElement ();
-				retval = 1;
-			} else if (!File.Exists (args [0])) {
-				writer.WriteStartElement ("error");
-				writer.WriteString ("File does not exist");
-				writer.WriteEndElement ();
-				retval = 2;
-			} else {
-				try {
-					FileVersionInfo fileInfo = FileVersionInfo.GetVersionInfo (Path.GetFullPath(args [0]));
-
-                    String companyName = fileInfo.CompanyName;
-
-                    if (companyName != null) {
-						writer.WriteStartElement("company");
-						writer.WriteString(xmlSanitize(companyName));
-						writer.WriteEndElement();
-					} else {
-                        writer.WriteStartElement("company");
-						writer.WriteString("UNKNOWN");
-						writer.WriteEndElement();
-                    }
-
-					writer.WriteStartElement ("product");
-					writer.WriteString (xmlSanitize(fileInfo.ProductName));
-					writer.WriteEndElement ();
-
-					writer.WriteStartElement ("version");
-					writer.WriteString (xmlSanitize(fileInfo.ProductVersion.ToString ()));
-					writer.WriteEndElement ();
+                    writeNode(writer, "CompanyName", fileInfo.CompanyName);
+                    writeNode(writer, "ProductName", fileInfo.ProductName);
+                    writeNode(writer, "ProductVersion", fileInfo.ProductVersion);
+                    writeNode(writer, "Comments", fileInfo.Comments);
+                    writeNode(writer, "FileDescription", fileInfo.FileDescription);
+                    writeNode(writer, "FileName", fileInfo.FileName);
+                    writeNode(writer, "FileVersion", fileInfo.FileVersion);
+                    writeNode(writer, "InternalName", fileInfo.InternalName);
+                    writeNode(writer, "LegalCopyright", fileInfo.LegalCopyright);
+                    writeNode(writer, "LegalTrademarks", fileInfo.LegalTrademarks);
+                    writeNode(writer, "OriginalFilename", fileInfo.OriginalFilename);
 
                     AssemblyName assemblyName = AssemblyName.GetAssemblyName(Path.GetFullPath(args[0]));
+                    writeNode(writer, "fullname", assemblyName.FullName);
 
+                }
+                catch (BadImageFormatException)
+                {
+                    writeNode(writer, "error", "Bad assembly file");
+                    retval = 3;
+                }
+                catch (FileLoadException)
+                {
+                    writeNode(writer, "error", "Managed assembly cannot be loaded");
+                    retval = 6;
+                }
+                catch (Exception e)
+                {
+                    writeNode(writer, "error", e.Message);
+                    retval = 5;
+                }
+            }
 
-                    writer.WriteStartElement ("fullname");
-					writer.WriteString(xmlSanitize(assemblyName.FullName));
-					writer.WriteEndElement();
+            writer.WriteEndElement();
+            writer.Close();
+            return retval;
+        }
+        private static void writeNode(XmlWriter writer, string name, string value)
+        {
+            if (value != null && value.Trim().Length > 0)
+            {
+                writer.WriteStartElement(name);
+                writer.WriteString(xmlSanitize(value));
+                writer.WriteEndElement();
+            }
+        }
+        private static string xmlSanitize(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (XmlConvert.IsXmlChar(c))
+                {
+                    result.Append(c);
+                }
+                else
+                {
+                    result.Append('?');
+                }
+            }
+            return result.ToString();
+        }
 
-					/*
-					writer.WriteStartElement ("types");
-					foreach (Type type in assembly.GetTypes()) {
-						writer.WriteStartElement ("type");
-						writer.WriteString (xmlSanitize(type.FullName));
-						writer.WriteEndElement ();
-					}
-					writer.WriteEndElement ();
-					*/
-				} catch (BadImageFormatException) {
-					writer.WriteStartElement ("error");
-					writer.WriteString ("Bad assembly file");
-					writer.WriteEndElement ();
-					retval = 3;
-				} catch (FileLoadException) {
-					writer.WriteStartElement ("error");
-					writer.WriteString ("Managed assembly cannot be loaded");
-					writer.WriteEndElement ();
-					retval = 6;
-				} catch (Exception e) {
-					writer.WriteStartElement ("error");
-					writer.WriteString (e.Message);
-					writer.WriteEndElement ();
-					retval = 5;
-				}
-			}
-
-			writer.WriteEndElement ();
-			writer.Close ();
-			return retval;
-		}
-	}
+    }
 }
