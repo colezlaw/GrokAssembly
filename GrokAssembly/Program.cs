b24363d36069510a4046383f2dab1024edb89ceb
Jeremy Long <jeremy.long@gmail.com>
2018-07-29 07:38:46 -0400

    fully adopt .net core

--- GrokAssembly/Program.cs
+++ GrokAssembly/Program.cs
@@ -1,3 +1,18 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2018 The OWASP Foundation. All Rights Reserved.
+ */
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,8 +23,19 @@ using System.Reflection;
 
 namespace GrokAssembly
 {
+    /// <summary>
+    /// The main class for the GrokAssembly executable.
+    /// Extracts extended properties from .NET assemblies and outputs it as
+    /// XML for use by OWASP dependency-check.
+    /// </summary>
     static class MainClass
     {
+        /// <summary>
+        /// The main method for the GrokAssembly executable.
+        /// Accepts a single file path to a .NET assembly and extracts extended
+        /// properties from .NET assemblies and outputs it as XML for use by
+        /// OWASP dependency-check.
+        /// </summary>
         public static int Main(string[] args)
         {
             int retval = 0;
@@ -21,15 +47,12 @@ namespace GrokAssembly
 
             if (args.Length != 1)
             {
-                writer.WriteStartElement("error");
-                writer.WriteString("Usage: GrokAssembly.exe <filename>");
-                writer.WriteEndElement();
+                writeNode(writer, "error", "Usage: GrokAssembly.exe <filename>");
                 retval = 1;
             }
             else if (!File.Exists(args[0]))
             {
-                writer.WriteStartElement("error");
-                writer.WriteString("File does not exist");
+                writeNode(writer, "error", "File does not exist");
                 writer.WriteEndElement();
                 retval = 2;
             }
@@ -76,6 +99,15 @@ namespace GrokAssembly
             writer.Close();
             return retval;
         }
+
+
+        /// <summary>
+        /// Writes a complete xml node to the output stream. If the value is
+        /// null or empty the node is not written.
+        /// </summary>
+        /// <param name="writer">A reference to an XMLWriter.</param>
+        /// <param name="name">The name of the xml node.</param>
+        /// <param name="value">The body text of the xml node..</param>
         private static void writeNode(XmlWriter writer, string name, string value)
         {
             if (value != null && value.Trim().Length > 0)
@@ -85,6 +117,15 @@ namespace GrokAssembly
                 writer.WriteEndElement();
             }
         }
+
+        /// <summary>
+        /// Sanitizes the string for output in XML. Any non-XML characters are
+        /// replaced by '?'.
+        /// </summary>
+        /// <returns>
+        /// A string that can safetly be written into XML.
+        /// </returns>
+        /// <param name="input">The string to sanitize.</param>
         private static string xmlSanitize(string input)
         {
             StringBuilder result = new StringBuilder();
